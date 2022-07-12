bfa21a5ac2cbcc4935495c8ae4b2d4043e93b8e5
hiverkiya <hiverkiya@gmail.com>
2022-07-12 17:23:41 +0530

    Implementing ClassComponents

---
+++ src/Details.js
@@ -0,0 +1,44 @@
+import {useParams} from "react-router-dom"
+import { Component } from "react"
+class Details extends Component{
+   /* constructor(props)
+    {
+        super(props)
+        this.state={loading:true}
+    }*/
+    state={loading:true}
+    aync componentDidMount(){
+        const res= await fetch(
+            `http://pets-v2.dev=apis.com/pets?id=${this.props.params.id}`
+        )
+        const json=await res.json()
+        this.setState(Object.assign({loading:false},json.pets[0]))
+        
+    }
+    render(){
+        if(this.state.loading){
+            return <h2>loading...</h2>
+        }
+        const {animal,breed,city,state,description,name}=this.state;
+        return(
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{animal} - {breed} - {city}, {state}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+/*const Details=()=>{
+    const {id}= useParams()
+    return <h2> {id}</h2>
+}*/
+const WrappedDetails=()=>{
+    const params=useParams()
+    return <Details params={params}/>
+}
+export default WrappedDetails
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,12 @@
 // import React from "react"
+import {Link} from "react-router-dom"
 const Pet = ({name,animal,breed,images,location,id}) =>{
 let hero = "http://pet-images.dev-apis.com/pets/none.jpg"
 if (images.length){
   hero=images[0]
 }
    return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       < div className="image-container">
         <img src={hero} alt={name}/>
       </div>
@@ -13,7 +14,7 @@ if (images.length){
         <h1>{name}</h1>
         <h2>{animal} - {breed} - {location}</h2>
       </div>
-    </a>
+    </Link>
    ) 
 } 
 /*
